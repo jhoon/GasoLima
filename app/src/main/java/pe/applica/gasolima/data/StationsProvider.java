a1223d9f5865e023b27665932d30a2fb64f7052a
Jhoon Saravia <jhoonsar@gmail.com>
2016-01-14 09:31:41 -0500

    chore: Update StationsProvider with method stubs
    Now we make it inherit from ContentProvider and implement its methods.

--- app/src/main/java/pe/applica/gasolima/data/StationsProvider.java
+++ app/src/main/java/pe/applica/gasolima/data/StationsProvider.java
@@ -1,8 +1,46 @@
 package pe.applica.gasolima.data;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
 /**
  * Provider for the Stations List and the Station Detail
  * Created by jhoon on 1/14/16.
  */
-public class StationsProvider {
+public class StationsProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
 }
