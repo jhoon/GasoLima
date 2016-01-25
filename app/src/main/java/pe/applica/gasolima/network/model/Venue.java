3a52efe99565d57155e429ad5c8f2d093a138ff1
Jhoon Saravia <jhoonsar@gmail.com>
2016-01-25 11:17:46 -0500

    chore: Add method to resolve the gases string

--- app/src/main/java/pe/applica/gasolima/network/model/Venue.java
+++ app/src/main/java/pe/applica/gasolima/network/model/Venue.java
@@ -20,5 +20,27 @@ public class Venue {
         public String GAS95;
         public String GAS97;
         public String GAS98;
+        public String generateGasesString() {
+            String result = "";
+            if (!GAS84.equals("-1")) {
+                result += "G84, ";
+            }
+            if (!GAS90.equals("-1")) {
+                result += "G90, ";
+            }
+            if (!GAS95.equals("-1")) {
+                result += "G95, ";
+            }
+            if (!GAS97.equals("-1")) {
+                result += "G97, ";
+            }
+            if (!GAS98.equals("-1")) {
+                result += "G98, ";
+            }
+            if (result.length() > 0) {
+                result = result.substring(0, result.length()-2);
+            }
+            return result;
+        }
     }
 }
