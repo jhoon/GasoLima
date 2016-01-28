f469f00e156c2812b3b6a983509b184b10656c2c
Jhoon Saravia <jhoonsar@gmail.com>
2016-01-28 14:01:49 -0500

    chore: Remove fab from main activity
    No need for one there

--- app/src/main/java/pe/applica/gasolima/GasStationListActivity.java
+++ app/src/main/java/pe/applica/gasolima/GasStationListActivity.java
@@ -95,15 +95,6 @@ public class GasStationListActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         View recyclerView = findViewById(R.id.gasstation_list);
         assert recyclerView != null;
         mRecyclerView = (RecyclerView)recyclerView;
