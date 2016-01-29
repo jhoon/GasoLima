87247fd4e2f79ff98960db7a4581e440d0130d8c
Jhoon Saravia <jhoonsar@gmail.com>
2016-01-28 23:32:54 -0500

    feat: Using ItemChoiceManager to persist choice
    Removed unused methods
    
    Made sure that the location is loaded once when the app
    starts and not every time the app changes configuration
    (such as when the screen is rotated)
    
    Saving the location and selected position to the outState
    so it can be retrieved when recreating the activity.

--- app/src/main/java/pe/applica/gasolima/GasStationListActivity.java
+++ app/src/main/java/pe/applica/gasolima/GasStationListActivity.java
@@ -18,6 +18,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.AbsListView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -57,13 +59,18 @@ public class GasStationListActivity extends AppCompatActivity
      * device.
      */
     private boolean mTwoPane;
+    private boolean mAutoSelectView;
     private GoogleApiClient mGoogleApiClient;
     private RecyclerView mRecyclerView;
     private Location mLastLocation;
     private StationsAdapter mStationsAdapter;
+    private int mPosition;
+
+    public static final String SELECTED_KEY = "selected_position";
+    public static final String CURRENT_LOCATION = "current_location";
 
     /**
-     * Identifier for the lader
+     * Identifier for the loader
      */
     private static final int STATIONS_LOADER = 0;
 
@@ -86,8 +93,19 @@ public class GasStationListActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gasstation_list);
+
+        if (findViewById(R.id.gasstation_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+            mAutoSelectView = true;
+        }
+
         buildGoogleApiClient();
-        mStationsAdapter = new StationsAdapter(this, this);
+        mStationsAdapter = new StationsAdapter(this, this,
+                mTwoPane? AbsListView.CHOICE_MODE_SINGLE:AbsListView.CHOICE_MODE_NONE);
 
         Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -98,15 +116,32 @@ public class GasStationListActivity extends AppCompatActivity
         mRecyclerView = (RecyclerView)recyclerView;
         mRecyclerView.setAdapter(mStationsAdapter);
 
-        if (findViewById(R.id.gasstation_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(SELECTED_KEY)) {
+                mPosition = savedInstanceState.getInt(SELECTED_KEY);
+            }
+            Log.i(TAG, "onCreate: savedInstanceState is not null!");
+            if (savedInstanceState.containsKey(CURRENT_LOCATION)) {
+                Log.i(TAG, "onCreate: let's get the parcelable!");
+                mLastLocation = savedInstanceState.getParcelable(CURRENT_LOCATION);
+            }
+            mStationsAdapter.onRestoreInstanceState(savedInstanceState);
         }
 
         getSupportLoaderManager().initLoader(STATIONS_LOADER, null, this);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if (mPosition != RecyclerView.NO_POSITION) {
+            outState.putInt(SELECTED_KEY, mPosition);
+        }
+        Log.i(TAG, "onSaveInstanceState: mLastLocation" + mLastLocation);
+        if (mLastLocation != null) {
+            outState.putParcelable(CURRENT_LOCATION, mLastLocation);
+        }
+        mStationsAdapter.onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
     }
 
     void buildGoogleApiClient() {
@@ -119,13 +154,15 @@ public class GasStationListActivity extends AppCompatActivity
         }
     }
 
-    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new StationsAdapter(this, this));
-    }
-
     @Override
     public void onConnected(Bundle bundle) {
+        if (mLastLocation == null) {
+            Log.i(TAG, "onConnected: IT'S UPDATING... WHY?! WHY?!");
+            updateLocation();
+        }
+    }
 
+    private void updateLocation() {
         // Check for Location Permissions
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -159,7 +196,7 @@ public class GasStationListActivity extends AppCompatActivity
                             int i = 0;
 
                             for (Venue venue : venues) {
-                                Log.i(TAG, "onResponse: venues~ " + venue.venue.id + " nombre: " + venue.venue.nombre);
+                                Log.d(TAG, "onResponse: venues~ " + venue.venue.id + " nombre: " + venue.venue.nombre);
 
                                 ContentValues stationValues = new ContentValues();
                                 stationValues.put(StationEntry.COLUMN_ID, venue.venue.id);
@@ -242,6 +279,28 @@ public class GasStationListActivity extends AppCompatActivity
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
         mStationsAdapter.swapCursor(data);
+        if (mPosition != RecyclerView.NO_POSITION) {
+            mRecyclerView.smoothScrollToPosition(mPosition);
+        }
+        if (data.getCount() > 0) {
+            mRecyclerView.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+                @Override
+                public boolean onPreDraw() {
+                    if (mRecyclerView.getChildCount() > 0) {
+                        mRecyclerView.getViewTreeObserver().removeOnPreDrawListener(this);
+                        int position = mStationsAdapter.getSelectedItemPosition();
+                        if (position == RecyclerView.NO_POSITION) position = 0;
+                        mRecyclerView.smoothScrollToPosition(position);
+                        RecyclerView.ViewHolder vh = mRecyclerView.findViewHolderForAdapterPosition(position);
+                        if (null != vh && mAutoSelectView) {
+                            mStationsAdapter.selectView(vh);
+                        }
+                        return true;
+                    }
+                    return false;
+                }
+            });
+        }
     }
 
     @Override
@@ -267,5 +326,6 @@ public class GasStationListActivity extends AppCompatActivity
 
             startActivity(intent);
         }
+        mPosition = vh.getAdapterPosition();
     }
 }
