379e1077c38616089ba6ef20502f128465ff13e7
Jhoon Saravia <jhoonsar@gmail.com>
2016-01-28 23:23:58 -0500

    chore: Optimize GasStationListActivity imports

--- app/src/main/java/pe/applica/gasolima/GasStationListActivity.java
+++ app/src/main/java/pe/applica/gasolima/GasStationListActivity.java
@@ -9,8 +9,6 @@ import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.LoaderManager.LoaderCallbacks;
 import android.support.v4.content.CursorLoader;
