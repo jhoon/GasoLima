a3b41c8e302ece4b3d60a2493ca20e849d0ece78
Jhoon Saravia <jhoonsar@gmail.com>
2016-01-30 17:06:34 -0500

    chore: Create AsyncTask

---
+++ app/src/main/java/pe/applica/gasolima/network/StationListTask.java
@@ -0,0 +1,15 @@
+package pe.applica.gasolima.network;
+
+import android.location.Location;
+import android.os.AsyncTask;
+
+/**
+ * AsyncTask that retrieves Gas Stations
+ * Created by jhoon on 1/30/16.
+ */
+public class StationListTask extends AsyncTask<Location, Void, Void>{
+    @Override
+    protected Void doInBackground(Location... params) {
+        return null;
+    }
+}
